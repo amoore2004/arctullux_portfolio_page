109a54e489c17f6258deb7b8f7e338cb0ef1135f
arctullux_04 <alex.b.moore@me.com>
2026-04-24 12:44:16 -0500

    Completed the functionality of the Work Experience Dropdown Menu.:

---
+++ assets/scripts/index.js
@@ -0,0 +1,28 @@
+/*
+Project Information:
+
+Started APR 20, 2026
+Code starts at initialization (bottom of script)
+
+*/
+
+// ========== Beginning Variables
+
+const MAIN_PAGE = '../pages/index.html';
+const WORK_EXPERIENCE_DROPDOWN_MENU_BUTTON = document.getElementById("work_experience_type_button");
+const EDUCATION_DROPDOWN_BUTTON = document.getElementById("education_type_button");
+
+// ========== Functions
+
+
+
+// ========== Initialization
+
+function init() {
+
+    console.log("Program starting...");
+}
+// ========== Event Listeners
+
+// ========== Init Function call
+init();
